dc289b6bf98af8d627d6e40d03af07f2274d65a5
Brandon Dusch <brandondusch@gmail.com>
2020-03-05 03:10:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  delegate_counts = {"Joe Biden" => }
+  delegate_counts = {"Joe Biden" => 566, "Bernie Sanders" => 501}
   
 end
 
